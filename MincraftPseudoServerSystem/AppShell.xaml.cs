b6a973b4ec58fb50e609072ddb5ca66f3993203a
biuntri <biuntri.213@gmail.com>
2026-04-03 12:09:25 +0900

    MAUIプロジェクトを新規作成

---
+++ MincraftPseudoServerSystem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MincraftPseudoServerSystem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MincraftPseudoServerSystem/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MincraftPseudoServerSystem
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
